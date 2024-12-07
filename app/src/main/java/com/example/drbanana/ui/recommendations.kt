36ec67fa2dbb77a510137188b668e7209b0fd3cc
rafaelmaghinay <rafaelfmaghinay@su.edu.ph>
2024-12-08 01:07:32 +0800

    update

--- app/src/main/java/com/example/drbanana/ui/recommendations.kt
+++ app/src/main/java/com/example/drbanana/ui/recommendations.kt
@@ -1,8 +1,14 @@
 package com.example.drbanana.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.drbanana.Disease
@@ -36,10 +42,16 @@ fun RecommendationsScreen(diseaseId: String?, navController: NavHostController) 
             }
         }
     }
-
-    if (disease != null) {
-        Text("Recommendations for disease: ${disease!!.treeDisease}, Image URI: ${disease!!.imageUri}")
-    } else {
-        Text("Loading...")
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        if (disease != null) {
+            Text("Recommendations for disease: ${disease!!.treeDisease}, Image URI: ${disease!!.imageUri}")
+        } else {
+            Text("Loading...")
+        }
     }
 }
